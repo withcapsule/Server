bb0874cf1c791998eae70b997634426e5a910cd1
Sean Singh <sean.9s9s0@gmail.com>
2026-05-25 19:56:24 -0700

    ping pong on port 9001

--- filemover-server/src/main.rs
+++ filemover-server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+    Json
+};
+
+use serde_json::{json, Value};
+
+async fn ping() -> Json<Value> {
+	return Json( json!( { "message": "pong" } ) )
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app: Router<> = Router::new()
+        .route("/", get( ping ) );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
